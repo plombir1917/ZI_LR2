05b7d916e563d2c1fd74e0cb675e6d2398880e3b
plombir1917 <dfhhggh@list.ru>
2024-03-06 11:15:35 +0300

    refactor: global

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import "./App.css";
-import "./index.css";
-import { useState } from "react";
+import './App.css';
+import './index.css';
+import { useState } from 'react';
 export default function App() {
-  const [key, setKey] = useState("");
-  const [text, setText] = useState("");
+  const [key, setKey] = useState('');
+  const [text, setText] = useState('');
 
-  const [result, setResult] = useState("");
-  const [resultTwo, setResultTwo] = useState("");
+  const [result, setResult] = useState('');
+  const [resultTwo, setResultTwo] = useState('');
 
   const changeTextHandler = (value) => {
     setText(value.target.value);
@@ -29,47 +29,44 @@ export default function App() {
 
   return (
     <>
-      <main className="w-screen h-screen bg-gray-800 p-5">
-        <div className="container mx-auto px-4 text-white bg-gray-600 min-h-full rounded-md ">
+      <main className='w-screen h-screen  p-5'>
+        <div className='container mx-auto px-4 text-gray bg-whitesmoke min-h-full'>
           <div
-            className="flex flex-col items-center "
-            style={{ height: "80vh" }}
+            className='flex flex-col items-center '
+            style={{ height: '80vh' }}
           >
-            <div className="text-5xl text-center mt-3">
+            <div className='text-5xl text-center mt-3 '>
               Защита информации. Лабораторная работа №2
             </div>
-            <div className="text-4xl mb-3 mt-1">
-              Выполнил Киселев Арсений. Разработано на React.js
-            </div>
-            <div className="flex ">
-              <div className="flex flex-col border-4 border-gray-800 p-3 rounded-md h-full mt-10">
-                <div className="text-3xl text-center mb-5 mt-1">
+            <div className='flex '>
+              <div className='flex flex-col p-3 rounded-md h-full mt-10 text-gray'>
+                <div className='text-3xl text-center mb-5 mt-1'>
                   Шифрование сообщения шифром DES в режиме ECB
                 </div>
                 <input
                   onChange={changeKeyHandler}
                   value={key}
-                  type="text"
-                  style={{ outline: "none" }}
-                  placeholder="Введите ключ"
-                  className="rounded text-black p-4 border-4 border-gray-800 my-5"
+                  type='text'
+                  style={{ outline: 'none' }}
+                  placeholder='Введите ключ'
+                  className='rounded text-gray p-4 border-4  my-5'
                 />
                 <input
-                  type="text"
-                  style={{ outline: "none" }}
+                  type='text'
+                  style={{ outline: 'none' }}
                   onChange={changeTextHandler}
                   value={text}
-                  placeholder="Введите текст"
-                  className="rounded border-4 border-gray-800 text-black p-4"
+                  placeholder='Введите текст'
+                  className='rounded border-4  text-black p-4'
                 />
                 <button
-                  className="w-full p-3 bg-white text-black border-4 border-gray-800 mt-5 hover:text-white hover:bg-black"
+                  className='w-full p-3 bg-white text-black border-4  mt-5 hover:text-white hover:bg-black'
                   onClick={calculateResultHandler}
                 >
                   Рассчитать
                 </button>
-                <div className="text-center mt-4 text-3xl">{result}</div>
-                <div className="text-center mt-4 text-3xl">{resultTwo}</div>
+                <div className='text-center mt-4 text-3xl'>{result}</div>
+                <div className='text-center mt-4 text-3xl'>{resultTwo}</div>
               </div>
             </div>
           </div>
